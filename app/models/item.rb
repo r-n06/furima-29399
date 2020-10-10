f2df7b19bf06199686843d09b4aca939f820f89f
r-n06 <19nr96.xx.o6@gmail.com>
2020-10-10 15:27:40 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,10 +9,12 @@ class Item < ApplicationRecord
   belongs_to_active_hash :status
   has_one_attached :image
 
-  validates :name, presence: true
-  validates :introduction, presence: true
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :category, :postage_payer, :shipping_area, :shipment_day, :status, presence: true
+  with_options presence: true do
+    validates :name
+    validates :introduction
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :category, :postage_payer, :shipping_area, :shipment_day, :status
+    validates :image
+  end
   validates :category_id, :postage_payer_id, :shipping_area_id, :shipment_day_id, :status_id, numericality: { other_than: 1 }
-  validates :image, presence: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -29,29 +29,29 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Introduction can't be blank")
       end
       it 'カテゴリーの選択ができていなければ投稿できない' do
-        @item.category_id = nil
+        @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category can't be blank")
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it '商品状態の選択ができていなければ投稿できない' do
-        @item.status_id = nil
+        @item.status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Status can't be blank")
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
       it '配送料の負担の選択ができていなければ投稿できない' do
-        @item.postage_payer_id = nil
+        @item.postage_payer_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Postage payer can't be blank")
+        expect(@item.errors.full_messages).to include("Postage payer must be other than 1")
       end
       it '発送元の地域の選択ができていなければ投稿できない' do
-        @item.shipping_area_id = nil
+        @item.shipping_area_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping area can't be blank")
+        expect(@item.errors.full_messages).to include("Shipping area must be other than 1")
       end
       it '発送までの日数の選択ができていなければ投稿できない' do
-        @item.shipment_day_id = nil
+        @item.shipment_day_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipment day can't be blank")
+        expect(@item.errors.full_messages).to include("Shipment day must be other than 1")
       end
       it '価格の情報がないと投稿できない' do
         @item.price = nil
@@ -62,6 +62,11 @@ RSpec.describe Item, type: :model do
         @item.price = '100'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it '価格設定が300円〜9,999,999円でないと投稿できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
       it '価格の入力が半角でないと投稿できない' do
         @item.price = '３００'
