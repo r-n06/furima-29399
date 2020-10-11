fddccb039cb9931c61863abe21fafbad52da09f1
r-n06 <19nr96.xx.o6@gmail.com>
2020-10-11 16:28:50 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, only: :edit
   before_action :set_item, only: [:show, :update]
 
   def index
