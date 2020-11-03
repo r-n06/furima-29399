f171febd958bae052bda7d32ca28fa7120af0337
r-n06 <19nr96.xx.o6@gmail.com>
2020-11-03 19:41:48 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'furima-29399'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:r-n06/furima-29399.git'
+set :repo_url,  'git@github.com/r-n06/furima-29399.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
